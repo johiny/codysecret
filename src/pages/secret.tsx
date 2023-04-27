013ead26339f05cf6670a2efd31c8b7250661a32
Johiny <johiny7@live.com>
2023-04-26 21:42:59 -0500

    arreglo de typo

--- src/pages/secret.tsx
+++ src/pages/secret.tsx
@@ -12,7 +12,7 @@ const Secret = ({session} : {session: Session}) => {
   return (
     <div className='flex min-h-screen flex-col justify-items-start items-center '>
        <h1 className='text-retroblue font-roboto text-6xl font-bold text-center z-10 leading-loose mt-10 animate-puffInTop delay-100'>A wey me hubieras dicho que eras tu {session?.user?.name || "paco"}<br/>
-       Bueno mi screto es... un chiste!<br/>
+       Bueno mi secreto es... un chiste!<br/>
        ¿Sabes por qué el cocodrilo programador no usa Windows?<br/> ¡Porque prefiere &quot;crocodear&quot; en Linux!</h1>
        <button className="bg-retroblue text-white font-roboto w-2/12 font-bold text-2xl p-2 mt-10 rounded-md animate-puffInTop delay-200 hover:bg-opacity-90" onClick={() => signOut()}>Salir de la sala secreta</button>
       <Image src={big_cody} height={500} width={500} alt="big_cody" className="absolute bottom-0 left-0 animate-puffInTop"/>
